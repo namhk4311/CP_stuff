f8027895431d34eea2a861593f76d33ada989c14
namhk4311 <khanhnamgalaxy@gmail.com>
2023-01-03 15:32:18 +0700

    watermelon 4A

---
+++ test_1-test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w;
+    cout << "weight: ";
+    cin >> w;
+    if (w == 1 || w == 2) cout << "No";
+    else {
+    for (int i = 1; i < w; i++)
+    {
+        if (i % 2 == 0)
+        {
+            if ((w-i) % 2 == 0)
+            {
+                cout << "Yes";
+                break;
+            }
+            else
+            {
+                cout << "No";
+                break;
+            }
+        }
+    }
+    }
+    return 0;
+}
