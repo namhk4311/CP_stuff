2d2cdf246b5ef413580106908dffd5298b80ff12
namhk4311 <khanhnamgalaxy@gmail.com>
2023-01-03 20:51:08 +0700

    fix unsucess

--- Way_too_long_words.cpp
+++ Way_too_long_words.cpp
@@ -1,27 +1,34 @@
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
+
+void checkint(string a)
+{
+    stringstream ss;
+    int num;
+    ss << a;
+    ss >> num;
+    if ( isdigit(num) == 0 )
+    {
+        exit(3);
+    }
+    else {cout << a;}
+}
 
 int main()
 {
-    int n, lth, p;
+    int n, lth;
     string x, k, t;
     cout << "Nhap: ";
     getline(cin,x);
-    p = stoi(x);
     lth = x.length();
-    if (lth >= 10)
+    if (lth < 10)
     {
-        t = to_string(lth-2);
+        checkint(x);
+    }
+    else 
+    {
+         t = to_string(lth-2);
         k = x[0] + t + x[lth-1]; 
         cout << k;
     }
-    else if (lth < 10 && p != 0) 
-    {
-        return 0;
-    }
-    else cout << x;
-
 }
