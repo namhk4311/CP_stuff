c16c4e44d4d03a813eac2927a02debc91407e618
namhk4311 <khanhnamgalaxy@gmail.com>
2023-03-26 21:07:29 +0700

    Wrong Subtraction

---
+++ 110A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long int uli;
+ 
+void solve()
+{
+    int n; cin >> n;
+    
+}
+
+int main()
+{
+    int test = 1; cin >> test;
+    while (test--) solve();
+    return 0;
+}
